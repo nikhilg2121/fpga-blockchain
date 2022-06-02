c4be41e8c5542436f1f7b0e3662ba61293a937e0
aniket-64 <96648292+aniket-64@users.noreply.github.com>
2022-06-03 00:56:29 +0530

    Delete halfadder.v

--- Q1/halfadder.v
+++
@@ -1,8 +0,0 @@
-module halfadder(input a,   //first bit
-                 input b,   //second bit
-                 output carry,  
-                 output o); //output sum
-    assign o = a ^ b;
-    assign carry = a & b;
-
-endmodule
