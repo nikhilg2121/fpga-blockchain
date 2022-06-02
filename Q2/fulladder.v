544aef98c989eb5adfb341fd73c717d5829b5299
aniket-64 <96648292+aniket-64@users.noreply.github.com>
2022-06-03 00:51:54 +0530

    Delete fulladder.v

--- Q2/fulladder.v
+++
@@ -1,9 +0,0 @@
-module fulladder(input a,   //first bit
-                 input b,   //second bit
-                 input cin, //input carry
-                 output cout, //output carry
-                 output o); //output sum
-    assign o = (a ^ b) ^ cin;
-    assign cout = (a & b) | (a | b) & cin;
-
-endmodule
